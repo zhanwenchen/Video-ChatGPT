d4d3f907c36edeaa1b26eb37fbbad62956c40a37
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2023-12-01 00:44:24 +0000

    Implemented eval for loo

--- video_chatgpt/eval/run_inference_tomloc_qa.py
+++ video_chatgpt/eval/run_inference_tomloc_qa.py
@@ -5,7 +5,8 @@ from json import load as json_load, dump as json_dump
 from glob import glob
 from warnings import warn
 from tqdm import tqdm
-from torch import device as torch_device, no_grad as torch_no_grad
+from torch import device as torch_device, no_grad as torch_no_grad, load as torch_load
+from torch.nn import Parameter
 from video_chatgpt.eval.model_utils import initialize_model, load_video
 from video_chatgpt.inference import video_chatgpt_infer
 
@@ -24,8 +25,37 @@ def parse_args():
     parser.add_argument("--model-name", type=str, required=True)
     parser.add_argument("--conv-mode", type=str, required=False, default='video-chatgpt_v1')
     parser.add_argument("--projection_path", type=str, required=False)
+    parser.add_argument("--loo_mm_projector_path", type=str, required=False)
 
     return parser.parse_args()
+
+# @torch_no_grad()
+# def evaluate_accuracy(model, device, dataloader_test) -> None:
+#     '''
+#     3. evalute on each video. Assume the dataloader batch size is 1. Model is not changed.
+#     '''
+#     model.eval()
+#     accuracy_per_vid = {}
+#     for batch in dataloader_test:
+#         vid = batch.pop('id')
+#         assert len(vid) == 1
+#         vid = vid[0]
+#         batch = {k: v.to(device, non_blocking=True) for k, v in batch.items()}
+#         _, logits = model(**batch, return_dict=False)
+#         breakpoint()
+#         # TODO
+#         accuracy = None
+#         labels = batch.pop('labels')
+#         accuracy_per_vid[vid] = accuracy
+
+#     return accuracy_per_vid
+
+@torch_no_grad()
+def load_nn_from_file(model, fname_weight_bias_frame_idx: str) -> None:
+    dict_weights_biases = torch_load(fname_weight_bias_frame_idx)
+    # model[0].weight = nn.Parameter(torch.ones_like(model[0].weight))
+    model.model.mm_projector.weight =  Parameter(dict_weights_biases['weight'].to(dtype=model.dtype)).to(dtype=model.dtype)
+    model.model.mm_projector.bias = Parameter(dict_weights_biases['bias'].to(dtype=model.dtype)).to(dtype=model.dtype)
 
 
 @torch_no_grad()
@@ -38,8 +68,10 @@ def run_inference(args):
     """
     # Initialize the model
     video_dir = args.video_dir
+    fname_weight_bias_frame_idx = args.loo_mm_projector_path
     model, vision_tower, tokenizer, image_processor, video_token_len = initialize_model(args.model_name,
                                                                                         args.projection_path)
+    load_nn_from_file(model, fname_weight_bias_frame_idx)
     # Load both ground truth file containing questions and answers
     with open(args.gt_file_qa) as file:
         gt_qa = json_load(file)
@@ -51,6 +83,7 @@ def run_inference(args):
 
     output_list = []  # List to store the output results
     conv_mode = args.conv_mode
+
 
     device = torch_device('cuda:0')
 
