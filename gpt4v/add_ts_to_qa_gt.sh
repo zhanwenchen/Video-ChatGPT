20c5fdabd7b83000e100d45134a6ababa8c731f3
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2024-04-10 23:25:43 -0400

    #71 Add timestamps and 3 other metrics to qa gt file

--- gpt4v/add_ts_to_qa_gt.sh
+++ gpt4v/add_ts_to_qa_gt.sh
@@ -1,3 +1,6 @@
-python /home/zhanwen/vtom/gpt4v/add_ts_to_qa_gt.py --gpt4v_result_dirpath data/gpt4v/result --qa_file_in /home/zhanwen/vtom/data/siq2/qa/qa_train.json --qa_file_out /home/zhanwen/vtom/data/siq2/qa/qa_train_with_ts.json
-python /home/zhanwen/vtom/gpt4v/add_ts_to_qa_gt.py --gpt4v_result_dirpath data/gpt4v/result --qa_file_in /home/zhanwen/vtom/data/siq2/qa/qa_val.json --qa_file_out /home/zhanwen/vtom/data/siq2/qa/qa_val_with_ts.json
-python /home/zhanwen/vtom/gpt4v/add_ts_to_qa_gt.py --gpt4v_result_dirpath data/gpt4v/result --qa_file_in /home/zhanwen/vtom/data/siq2/qa/qa_test.json --qa_file_out /home/zhanwen/vtom/data/siq2/qa/qa_test_with_ts.json
+export SPLIT=train # train, val, test
+python /home/zhanwen/vtom/gpt4v/add_ts_to_qa_gt.py --gpt4v_result_dirpath /home/zhanwen/vtom/gpt4v/result_${SPLIT} --qa_file_in /home/zhanwen/vtom/data/siq2/qa/qa_${SPLIT}.json --qa_file_out /home/zhanwen/vtom/data/siq2/qa/qa_${SPLIT}_with_ts.json
+export SPLIT=val # train, val, test
+python /home/zhanwen/vtom/gpt4v/add_ts_to_qa_gt.py --gpt4v_result_dirpath /home/zhanwen/vtom/gpt4v/result_${SPLIT} --qa_file_in /home/zhanwen/vtom/data/siq2/qa/qa_${SPLIT}.json --qa_file_out /home/zhanwen/vtom/data/siq2/qa/qa_${SPLIT}_with_ts.json
+export SPLIT=test # train, val, test
+python /home/zhanwen/vtom/gpt4v/add_ts_to_qa_gt.py --gpt4v_result_dirpath /home/zhanwen/vtom/gpt4v/result_${SPLIT} --qa_file_in /home/zhanwen/vtom/data/siq2/qa/qa_${SPLIT}.json --qa_file_out /home/zhanwen/vtom/data/siq2/qa/qa_${SPLIT}_with_ts.json
